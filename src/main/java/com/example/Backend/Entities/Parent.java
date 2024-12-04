e84524d2772da0638856ae1ebfd8299015c067b6
karmisonia <sonia.karmi@esprit.tn>
2024-12-04 11:25:40 +0100

    ajouter dependency

--- src/main/java/com/example/Backend/Entities/Parent.java
+++ src/main/java/com/example/Backend/Entities/Parent.java
@@ -1,21 +1,23 @@
 package com.example.Backend.Entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
-
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @DiscriminatorValue("Parent")
-public class Parent extends Utilisateur {
+public class Parent extends Utilisateur{
     @OneToOne
     @JoinColumn(name = "etudiant", referencedColumnName = "id")
     private Etudiant etudiant;
 }
+
+
